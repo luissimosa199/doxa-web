459e20e1aa8054c5ef977549d4f748aee60b6b2c
Luis <simosa37@gmail.com>
2024-01-09 16:59:47 -0300

    fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Fulix Software",
+  title: "Faulix Software",
   description: "Soluciones digitales",
 };
 
