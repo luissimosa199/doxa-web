77818f73518eed11ffc2ba3b013259e4426ad82b
Luis <simosa37@gmail.com>
2024-01-11 11:51:17 -0300

    metadata updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ export const metadata: Metadata = {
   title: "Faulix Software",
   description: "Soluciones digitales",
   metadataBase: new URL("https://faulix.com"),
+  openGraph: {
+    type: "website",
+    url: "https://faulix.com",
+    title: "Faulix Software",
+    description: "Soluciones digitales",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dahu3rii0/image/upload/c_fill,h_630,w_1200,f_png/v1704984495/FS_wwkkak.png",
+        width: 1200,
+        height: 630,
+        alt: "Faulix Software",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
